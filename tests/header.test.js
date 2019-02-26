0d30cfd143f21f4d9c770ee4259a976010323fd4
Paz Jaldety <paz@coperato.local>
2019-02-26 11:49:40 +0200

    Forgot a few commits

---
+++ tests/header.test.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+
+const sessionFactory = require('./factories/sessionFactory');
+
+let browser;
+let page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test('the header has the correct text', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(text).toEqual('Blogster');
+});
+
+test('clicking log in starts OAuth flow' , async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in shows logout button', async () => {
+  
+
+  const {session , sig} = sessionFactory();
+
+  await page.setCookie({ name: 'session', value: session });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
+
+  await page.waitFor('a[href="/auth/logout"');
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+  expect(text).toEqual('Logout');
+});
